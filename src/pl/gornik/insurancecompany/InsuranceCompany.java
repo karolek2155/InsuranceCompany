95169f06c50b5647d277fa7ba2a1ba20123aa1d4
karolek2155 <paz.karol.glowne@gmail.com>
2025-02-18 22:22:06 +0100

    Add user menu

--- src/pl/gornik/insurancecompany/InsuranceCompany.java
+++ src/pl/gornik/insurancecompany/InsuranceCompany.java
@@ -8,6 +8,8 @@ import java.util.List;
 public class InsuranceCompany {
     private List<Policy> policies = new ArrayList<>();
     private List<Client> clients = new ArrayList<>();
+    private List<ClaimReport> claimReports = new ArrayList<>();
+    private List<Payment> payments = new ArrayList<>();
 
     public void addPolicy(Policy policy) {
         policies.add(policy);
@@ -17,11 +19,27 @@ public class InsuranceCompany {
         clients.add(client);
     }
 
+    public void addClaimReport(ClaimReport claimReport) {
+        claimReports.add(claimReport);
+    }
+
+    public void addPayment(Payment payment) {
+        payments.add(payment);
+    }
+
     public List<Policy> getPolicies() {
         return policies;
     }
 
     public List<Client> getClients() {
         return clients;
+    }
+
+    public List<ClaimReport> getClaimReports() {
+        return claimReports;
+    }
+
+    public List<Payment> getPayments() {
+        return payments;
     }
 }
--- src/pl/gornik/insurancecompany/Main.java
+++ src/pl/gornik/insurancecompany/Main.java
@@ -9,50 +9,85 @@ import pl.gornik.insurancecompany.policies.Policy;
 import pl.gornik.insurancecompany.policies.PropertyInsurancePolicy;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+    private static InsuranceCompany insuranceCompany = new InsuranceCompany();
+
     public static void main(String[] args) {
-        InsuranceCompany insuranceCompany = new InsuranceCompany();
         System.out.println("System zarządzania polisami ubezpieczeniowymi, klientami, zgłoszeniami roszczeń oraz historią polis.");
 
-        initializeData(insuranceCompany);
+        Scanner scanner = new Scanner(System.in);
+        initializeData();
+        boolean isActive = true;
 
-        List<Policy> policies = insuranceCompany.getPolicies();
+        while (isActive) {
+            System.out.println("Co chcesz zrobić? (wpisz numer)");
+            System.out.println("1 - Złożyć wniosek o ubezpieczenie");
+            System.out.println("2 - Wyszukać roszczenie");
+            System.out.println("3 - Wyszukać klienta");
+            System.out.println("4 - Dodać klienta");
+            System.out.println("5 - Usunąć klienta");
+            System.out.println("6 - Wyszukać polisę");
+            System.out.println("7 - Wystawić polisę");
+            System.out.println("8 - Wyświetlić płatności");
+            System.out.println("9 - Wyświetlić składkę polisy");
+            System.out.println("0 - Wyjście");
 
-        List<ClaimReport> claimReports = new ArrayList<>();
-        claimReports.add(new ClaimReport(insuranceCompany.getClients().get(0), "Uszkodzenie samochodu w wypadku", LocalDate.of(2024, 1, 16), policies.get(0).getPolicyNumber()));
-        claimReports.add(new ClaimReport(insuranceCompany.getClients().get(1), "Powódź w mieszkaniu", LocalDate.of(2024, 2, 21), policies.get(1).getPolicyNumber()));
-        claimReports.add(new ClaimReport(insuranceCompany.getClients().get(2), "Śmierć w wyniku wypadku", LocalDate.of(2024, 3, 12), policies.get(2).getPolicyNumber()));
-        claimReports.add(new ClaimReport(insuranceCompany.getClients().get(3), "Uszkodzenie samochodu w kolizji", LocalDate.of(2024, 4, 6), policies.get(3).getPolicyNumber()));
-        claimReports.add(new ClaimReport(insuranceCompany.getClients().get(4), "Pożar w budynku gospodarczym", LocalDate.of(2024, 5, 26), policies.get(4).getPolicyNumber()));
+            int choice = scanner.nextInt();
+            scanner.nextLine();
 
-        List<Payment> payments = new ArrayList<>();
-        payments.add(new Payment(1200, PaymentMethod.CASH));
-        payments.add(new Payment(880, PaymentMethod.BLIK));
-        payments.add(new Payment(1400, PaymentMethod.CARD));
-        payments.add(new Payment(950, PaymentMethod.CASH));
-        payments.add(new Payment(700, PaymentMethod.BLIK));
+            switch (choice) {
+                case 1 -> {
+                    System.out.println("Składanie wniosku o ubezpieczenie...");
+                }
+                case 2 -> {
+                    System.out.println("Wyszukiwanie roszczeń...");
+                    System.out.println("\nRaporty roszczeń:");
+                    for (ClaimReport report : insuranceCompany.getClaimReports()) {
+                        System.out.println(report);
+                    }                }
+                case 3 -> {
+                    System.out.println("Wyszukiwanie klienta...");}
+                case 4 -> {
+                    System.out.println("Dodawanie nowego klienta...");
+                }
+                case 5 -> {
+                    System.out.println("Usuwanie klienta...");
+                }
+                case 6 -> {
+                    System.out.println("Wyszukiwanie polisy...");
+                }
+                case 7 -> {
+                    System.out.println("Wystawianie nowej polisy...");
+                }
+                case 8 -> {
+                    System.out.println("Wyszukiwanie płatności...");
+                    System.out.println("\nPłatności:");
+                    for (Payment payment : insuranceCompany.getPayments()) {
+                        System.out.println(payment);
+                    }
+                }
+                case 9 -> {
+                    System.out.println("Obliczanie składek...");
+                    System.out.println("\nSkładki dla polis:");
+                    for (Policy policy : insuranceCompany.getPolicies()) {
+                        policy.updatePremium();
+                        System.out.printf("Polisa (%s) o numerze %s: %.2f\n", policy.getInsuranceType().getDisplayName(), policy.getPolicyNumber(), policy.getPremium());
+                    }
+                }
+                case 0 -> {
+                    isActive = false;
+                    System.out.println("Zakończono działanie systemu.");
+                }
+                default -> System.out.println("Niepoprawny wybór, spróbuj ponownie.");
+            }
 
-        System.out.println("\nSkładki dla polis:");
-        for (Policy policy : policies) {
-            policy.updatePremium();
-            System.out.printf("Polisa (%s) o numerze %s: %.2f\n", policy.getInsuranceType().getDisplayName(), policy.getPolicyNumber(), policy.getPremium());
-        }
-
-        System.out.println("\nRaporty roszczeń:");
-        for (ClaimReport report : claimReports) {
-            System.out.println(report);
-        }
-
-        System.out.println("\nPłatności:");
-        for (Payment payment : payments) {
-            System.out.println(payment);
         }
     }
 
-    public static void initializeData(InsuranceCompany insuranceCompany) {
+    public static void initializeData() {
         insuranceCompany.addClient(new Client("Jan", "Kowalski", "Warszawa, Mickiewicza 1"));
         insuranceCompany.addClient(new Client("Anna", "Nowak", "Kraków, Słoneczna 15"));
         insuranceCompany.addClient(new Client("Paweł", "Wiśniewski", "Gdańsk, Morska 3"));
@@ -64,5 +99,18 @@ public class Main {
         insuranceCompany.addPolicy(new LifeInsurancePolicy("L54321", insuranceCompany.getClients().get(2), 1200, LocalDate.of(2024, 6, 10), 100000));
         insuranceCompany.addPolicy(new AutoInsurancePolicy("A98765", insuranceCompany.getClients().get(3), 900, LocalDate.of(2024, 10, 5), AutoInsuranceType.COLLISION));
         insuranceCompany.addPolicy(new PropertyInsurancePolicy("P11223", insuranceCompany.getClients().get(4), 700, LocalDate.of(2024, 5, 25), PropertyInsuranceType.FARM));
+
+        List<Policy> policies = insuranceCompany.getPolicies();
+        insuranceCompany.addClaimReport(new ClaimReport(insuranceCompany.getClients().get(0), "Uszkodzenie samochodu w wypadku", LocalDate.of(2024, 1, 16), policies.get(0).getPolicyNumber()));
+        insuranceCompany.addClaimReport(new ClaimReport(insuranceCompany.getClients().get(1), "Powódź w mieszkaniu", LocalDate.of(2024, 2, 21), policies.get(1).getPolicyNumber()));
+        insuranceCompany.addClaimReport(new ClaimReport(insuranceCompany.getClients().get(2), "Śmierć w wyniku wypadku", LocalDate.of(2024, 3, 12), policies.get(2).getPolicyNumber()));
+        insuranceCompany.addClaimReport(new ClaimReport(insuranceCompany.getClients().get(3), "Uszkodzenie samochodu w kolizji", LocalDate.of(2024, 4, 6), policies.get(3).getPolicyNumber()));
+        insuranceCompany.addClaimReport(new ClaimReport(insuranceCompany.getClients().get(4), "Pożar w budynku gospodarczym", LocalDate.of(2024, 5, 26), policies.get(4).getPolicyNumber()));
+
+        insuranceCompany.addPayment(new Payment(1200, PaymentMethod.CASH));
+        insuranceCompany.addPayment(new Payment(880, PaymentMethod.BLIK));
+        insuranceCompany.addPayment(new Payment(1400, PaymentMethod.CARD));
+        insuranceCompany.addPayment(new Payment(950, PaymentMethod.CASH));
+        insuranceCompany.addPayment(new Payment(700, PaymentMethod.BLIK));
     }
 }
