5bc1a6019d82d610922505068c3740e3353b521e
karolek2155 <paz.karol.glowne@gmail.com>
2025-03-07 21:03:12 +0100

    Add generator test data

--- src/pl/gornik/insurancecompany/tools/TestDataGenerator.java
+++ src/pl/gornik/insurancecompany/tools/TestDataGenerator.java
@@ -22,7 +22,20 @@ public class TestDataGenerator {
         insuranceCompany.addClient(new Client("Paweł", "Wiśniewski", "Gdańsk, Morska 3", "23456789012", "345-678-901", "pawel.wisniewski@gmail.com"));
         insuranceCompany.addClient(new Client("Maria", "Wójcik", "Poznań, Pogodna 2", "34567890123", "456-789-012", "maria.wojcik@gmail.com"));
         insuranceCompany.addClient(new Client("Tomasz", "Zieliński", "Wrocław, Spokojna 7", "45678901234", "567-890-123", "tomasz.zielinski@gmail.com"));
-        insuranceCompany.addClient(new Client("Tomasz", "Zieliński", "Tarnobrzeg, Konopnickiej 21", "98678301238", "444-890-121", "t.zielinski@gmail.com"));
+        insuranceCompany.addClient(new Client("Katarzyna", "Lewandowska", "Łódź, Kwiatowa 9", "56789012345", "678-901-234", "katarzyna.lewandowska@gmail.com"));
+        insuranceCompany.addClient(new Client("Marek", "Dąbrowski", "Szczecin, Jasna 5", "67890123456", "789-012-345", "marek.dabrowski@gmail.com"));
+        insuranceCompany.addClient(new Client("Monika", "Mazur", "Lublin, Lipowa 3", "78901234567", "890-123-456", "monika.mazur@gmail.com"));
+        insuranceCompany.addClient(new Client("Robert", "Kaczmarek", "Bydgoszcz, Długa 10", "89012345678", "901-234-567", "robert.kaczmarek@gmail.com"));
+        insuranceCompany.addClient(new Client("Ewa", "Pawlak", "Rzeszów, Zielona 12", "90123456789", "012-345-678", "ewa.pawlak@gmail.com"));
+        insuranceCompany.addClient(new Client("Jacek", "Sikora", "Opole, Miodowa 7", "11223344556", "123-987-654", "jacek.sikora@gmail.com"));
+        insuranceCompany.addClient(new Client("Agnieszka", "Król", "Kielce, Sosnowa 15", "22334455667", "234-876-543", "agnieszka.krol@gmail.com"));
+        insuranceCompany.addClient(new Client("Karol", "Górski", "Toruń, Orla 8", "33445566778", "345-765-432", "karol.gorski@gmail.com"));
+        insuranceCompany.addClient(new Client("Dorota", "Lis", "Radom, Wiosenna 4", "44556677889", "456-654-321", "dorota.lis@gmail.com"));
+        insuranceCompany.addClient(new Client("Grzegorz", "Czajka", "Gdynia, Wodna 9", "55667788990", "567-543-210", "grzegorz.czajka@gmail.com"));
+        insuranceCompany.addClient(new Client("Beata", "Sadowska", "Koszalin, Polna 11", "66778899001", "678-432-109", "beata.sadowska@gmail.com"));
+        insuranceCompany.addClient(new Client("Łukasz", "Wrona", "Elbląg, Chmielna 2", "77889900112", "789-321-098", "lukasz.wrona@gmail.com"));
+        insuranceCompany.addClient(new Client("Natalia", "Stępień", "Częstochowa, Leśna 6", "88990011223", "890-210-987", "natalia.stepien@gmail.com"));
+        insuranceCompany.addClient(new Client("Wojciech", "Kołodziej", "Zielona Góra, Różana 14", "99001122334", "901-109-876", "wojciech.kolodziej@gmail.com"));
 
         insuranceCompany.addPolicy(new AutoInsurancePolicy("A12345", insuranceCompany.getClients().get(0), 1000, LocalDate.of(2025, 2, 17), AutoInsuranceType.COMPREHENSIVE));
         insuranceCompany.addPolicy(new PropertyInsurancePolicy("P67890", insuranceCompany.getClients().get(1), 800, LocalDate.of(2025, 1, 23), PropertyInsuranceType.HOME));
@@ -49,6 +62,24 @@ public class TestDataGenerator {
         authService.registerUser(new ClientUser("maria.wojcik@gmail.com", "kak67jsj"));
         authService.registerUser(new ClientUser("tomasz.zielinski@gmail.com", "zdf775"));
         authService.registerUser(new EmployeeUser("janek.kowalski@wp.pl", "emp456"));
+        authService.registerUser(new EmployeeUser("marcin.nowak@wp.pl", "emp656"));
+        authService.registerUser(new EmployeeUser("zuzanna.chwalek@wp.pl", "emp478"));
         authService.registerUser(new AdminUser("admin@gmail.com", "admin789"));
+
+        AutoInsuranceType[] autoTypes = AutoInsuranceType.values();
+        PropertyInsuranceType[] propertyTypes = PropertyInsuranceType.values();
+        PaymentMethod[] paymentMethods = PaymentMethod.values();
+
+        for (int i = 5; i < insuranceCompany.getClients().size(); i++) {
+            if (i % 2 == 0) {
+                insuranceCompany.addPolicy(new AutoInsurancePolicy("A" + (10000 + i), insuranceCompany.getClients().get(i), 800 + (i * 75), LocalDate.of(2025, (i % 12) + 1, (i % 28) + 1), autoTypes[i % autoTypes.length]));
+            } else {
+                insuranceCompany.addPolicy(new PropertyInsurancePolicy("P" + (20000 + i), insuranceCompany.getClients().get(i), 900 + (i * 60), LocalDate.of(2025, (i % 12) + 1, (i % 28) + 1), propertyTypes[i % propertyTypes.length]));
+            }
+
+            insuranceCompany.addClaimReport(new ClaimReport(insuranceCompany.getClients().get(i), "Zgłoszenie szkody nr " + i, LocalDate.of(2024, (i % 12) + 1, (i % 28) + 1), (i % 2 == 0 ? "A" : "P") + (10000 + i)));
+            insuranceCompany.addPayment(new Payment(900 + (i * 55), paymentMethods[i % paymentMethods.length]));
+            authService.registerUser(new ClientUser(insuranceCompany.getClients().get(i).getEmail(), "haslo" + i));
+        }
     }
 }
